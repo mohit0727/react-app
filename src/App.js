39ab0d03c1502675caa430879babb55a89333713
mohit.mittal <mohit.mittal@provakil.com>
2023-08-04 00:40:17 +0530

    Add routes in react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes } from "react-router-dom";
+import AddPage from "./components/AddPage";
+import Admin from "./components/Admin";
+import { Box, CssBaseline } from "@mui/material";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        <Box  sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/addPage" element={<AddPage />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </Box>
+      </div>
+    </>
   );
 }
 
